6c0399f4028c77ddbd693fc9985a2e8f8887154e
zivkovicbogdana <bogdanaz207@gmail.com>
2022-12-16 18:39:29 +0100

    Line 8 visualized headways for stops

--- sql/clustering.sql
+++ sql/clustering.sql
@@ -1,2 +1,7 @@
 select *, EXTRACT(epoch FROM headway)/60 as headways_minutes, EXTRACT(epoch FROM interval_start) as interval_start_seconds from headways;
 
+select *, EXTRACT(epoch FROM headway) as headway_seconds,  EXTRACT(epoch FROM interval_start) as interval_start_seconds from headways
+	INNER JOIN routes USING(route_id)
+	INNER JOIN stops USING(stop_id)
+	INNER JOIN calendar USING(service_id);
+
