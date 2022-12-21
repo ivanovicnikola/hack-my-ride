775a92f0bcbdef097cac48bc1d3764b4a124e16a
zivkovicbogdana <bogdanaz207@gmail.com>
2022-12-21 18:09:14 +0100

    fix

--- sql/delays.sql
+++ sql/delays.sql
@@ -1,6 +1,6 @@
 --analyze total delays for all trips on line 8, direction Roodebeek, weekday 9 september 2021
 SELECT trip_id, MIN(scheduled_time) first_stop_time, MAX(scheduled_time) last_stop_time, SUM(actual_time - scheduled_time) total_delay
-FROM scheduled_actual 
+FROM scheduled_actual_predicted 
 WHERE trip_headsign = 'ROODEBEEK' AND actual_time > scheduled_time
 GROUP BY trip_id 
 ORDER BY SUM(actual_time - scheduled_time) DESC;
@@ -9,7 +9,7 @@ ORDER BY SUM(actual_time - scheduled_time) DESC;
 
 --analyse the groups
 SELECT stop_sequence, stop_name, SUM(actual_time - scheduled_time)
-FROM scheduled_actual 
+FROM scheduled_actual_predicted 
 WHERE trip_headsign = 'ROODEBEEK' AND scheduled_time >= '19:20:00' AND scheduled_time <= '19:50:00' AND actual_time > scheduled_time
 GROUP BY stop_sequence, stop_name
 ORDER BY stop_sequence;
